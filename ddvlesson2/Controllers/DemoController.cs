bb2b628b490eb8e12460a566733bd29ad630c87e
ducvan186 <ducvan182006@gmail.com>
2026-08-24 00:10:23 +0700

    l2

---
+++ ddvlesson2/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ddvlesson2.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
